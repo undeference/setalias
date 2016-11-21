a6d3eb1576509309e58735657bb288ff7e155b73
msk <mkpdev@gmail.com>
2016-11-21 17:56:57 -0500

    GPL and GPL boilerplate

--- setalias.c
+++ setalias.c
@@ -1,3 +1,20 @@
+/*
+ * This file is part of setalias
+ * Copyright © 2016 M. Kristall
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of version 2 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of version 2 of the GNU General Public
+ * License with this program; if not, write to the Free Software Foundation, Inc
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
